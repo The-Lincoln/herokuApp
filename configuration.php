5ee10e29ad38f450767b3556a513bb164a4c97a2
MD.Shoeb Lincoln <md.s.lincoln@gmail.com>
2021-08-03 19:23:16 +0600

    Add files via upload

---
+++ configuration.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * Here all common required details related to account will be stored
+ */
+
+define('API_KEY', 'aNjlw0r1Q3CHVxkztTLwcg');
+
+define('API_SECRET', 'b3ywWkddwmOojbZKBP8gHuFZCVDMbTSv2Fny');
+
+define('API_URL', 'http://localhost');
+
+?>
